c259beb4d1af5a7d56714a359b716c0edb099cbe
Tasos Soukoulis <soukoulis.tasos@gmail.com>
2020-07-28 09:43:47 +0300

    Adding more logging to check what happen

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const github = require('@actions/github');
 try {
   console.log("Start validation")
   const prereleaseInput = core.getInput("prerelease");
+  console.log(prereleaseInput)
+  const ctx = github.context
+  console.log(ctx)
+  // github.getOctokit().git.get
 
-  core.branch
-
-  const branch=$()
 } catch (error) {
   core.setFailed(error.messsage)
 }
