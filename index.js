d3f0f6efde264ee28e538825fcea27c452e29c41
Tasos Soukoulis <soukoulis.tasos@gmail.com>
2020-07-28 10:38:12 +0300

    first hopefully working version

--- index.js
+++ index.js
@@ -2,12 +2,31 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  console.log("Start validation")
-  const prereleaseInput = core.getInput("prerelease");
-  console.log(prereleaseInput)
+  console.log("Start tag validation")
+  // const prereleaseInput = core.getInput("PRERELEASE");
   const ctx = github.context
-  console.log(ctx)
-  // github.getOctokit().git.get
+
+  const branch_name = ctx.payload.release.target_commitish;
+  const is_prerelease = ctx.payload.release.prerelease;
+  const release_version = ctx.payload.release.tag_name;
+
+  const master_branch_name = "master"
+  const release_prefix = "r-";
+
+  const is_master_branch = master_branch_name === branch_name;
+
+  const is_release_version = release_version.startsWith(release_prefix)
+
+  if (!is_prerelease && is_master_branch && is_release_version) {
+    core.info("Release tag validation succeeded")
+    return 0
+  } else if (is_prerelease) {
+    core.info("Pre-Release tag validation succeeded")
+    return 0
+  } else {
+    core.setFailed("Tag validation failed")
+    return 1
+  }
 
 } catch (error) {
   core.setFailed(error.messsage)
